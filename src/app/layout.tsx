5c90821521557a7fb8579c92525ba1ed759ec87d
Irish Catungal <irishclaireparayno@gmail.com>
2026-02-21 16:36:30 -0800

    google analyitics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -103,6 +103,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HPJSC9XB77" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-HPJSC9XB77');
+            `,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
